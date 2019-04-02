0cb63ae19741dd60b5cec774321e31aabe7b3332
LuisJairA <48858606+LuisJairA@users.noreply.github.com>
2019-04-01 21:43:30 -0500

    Add files via upload

---
+++ ejercicios.cpp
@@ -0,0 +1,57 @@
+2.3
+a) int c, estaEsUnaVariable, q76354, numero;
+b) std::cout << "Escriba un entero";
+c)std::cin >> edad;
+d)if ( numero  i=7 )
+     std::cout << "La variable numero no es igual a 7\n"
+e)std::cout << "Este es un programa en C++\n";
+f)std::cout << "Este es un \n programa en C++\n";
+g)std::cout << "Este\nes\nun\nprograma\nen\nC++\n";
+h)std::cout << "Este\tes\tun\tprograma\ten\tC++\n";
+
+
+2.4
+a) //Calcula el producto de tres enteros
+b) int x = 0;
+int y = 0;
+int z = 0;
+int resultado = 0;
+c) cout << "Escribe tres enteros:";
+d) cin >> x >> y >> z;
+e) resultado = x * y*z;
+f)cout << "El producto es" << resultado << endl
+g)return 0;
+
+
+2.5
+//Calculo el producto de tres enteros
+#include <iostream> //permite al programa realizar operaciones de entrada y salida
+using namespace std; //el programa usa nombres del std namespace
+
+//la función main empieza la ejecuación del programa
+int main()
+{
+	int x = 0; // primer entero a multiplicar
+	int y = 0; //segundo entero a multiplicar
+	int z = 0; //tercer entero a mmultiplicar
+	int resultado = 0; // el producto de los tres enteros
+
+	cout << "Escriba tres enteros"; // pide los datos al usuario
+	cin >> x >> y >> z; // lee tres enteros del usuario
+	resultado = x * y*z; //multiplica los tres enteros ; almacena el resultado
+	cout << "El producto es" << resultado << endl; //imprime el resultado; fin de línea
+}
+
+
+
+
+2.9
+a) count << "Escribir dos números";
+b) cin >> a;
+   cin >> b;
+   cin >> c;
+c)resultado = a + b + c; //sumatoria de los datos
+d)cin >> a;
+  cin >> b;
+  cin >> c;
+  cout<< a , b ,c
